bf7cd26f818f291ae25fc808f11dc18b925edac9
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-06-27 15:49:46 -0500

    a migration for creating a nerd table

--- app/database/migrations/2014_06_27_204257_create_nerds_table.php
+++ app/database/migrations/2014_06_27_204257_create_nerds_table.php
@@ -12,9 +12,12 @@ class CreateNerdsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::table('nerds', function(Blueprint $table)
-		{
-			//
+		Schema::table('nerds', function(Blueprint $table){
+      $table->increments('id');
+      $table->string('name', 255);
+      $table->string('email', 255);
+      $table->integer('nerd_level');
+      $table->timestamps();
 		});
 	}
 
